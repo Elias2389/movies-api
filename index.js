6cc85206b275ab0ac94975db07397f6d897244ba
Andres <andres.rivas@mercadolibre.com>
2020-02-09 11:39:35 -0300

    Added error handler and validation handler

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const  app = express();
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
+const { logError, errorHandler } = require('./utils/middleware/errorHandler');
+
 
 app.use(express.json());
 
 moviesApi(app);
 
+app.use(logError);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`);
